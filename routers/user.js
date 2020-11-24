d9848a04f5117b5abcccf7fdfd657c9c3cf50585
Chika Onyema <chika.onyema01@gmail.com git config --gobal user.name Chika>
2020-11-24 22:17:58 +0100

    console.logs-add

--- routers/user.js
+++ routers/user.js
@@ -20,7 +20,9 @@ router.post('/users', async  (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
+        console.log(user)
         sendEmail(user.email, user.firstName)
+        console.log(user.email, user.firstName)
         res.json({ result: "success", message: "Register successfully" }) 
     } catch (e) {
        res.status(400).send(e)
